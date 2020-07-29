51c6954127a6f7c6c0773a4cd0805e874acd9bf0
TyranitarX <tyranitarx@163.com>
2020-07-29 18:21:51 +0800

    init develop

---
+++ src/main/kotlin/com/github/CodingPokemon/Kayru/Utils/HttpUtil.kt
@@ -0,0 +1,45 @@
+package com.github.CodingPokemon.Kayru.Utils
+
+import com.alibaba.fastjson.JSON
+import com.alibaba.fastjson.JSONObject
+import net.dreamlu.mica.http.HttpRequest
+import net.mamoe.mirai.utils.DefaultLogger
+
+class HttpUtil() {
+    companion object {
+        /**
+         * 夸人
+         */
+        fun getKua(): String {
+            val url = "https://chp.shadiao.app/api.php"
+            return HttpRequest.get(url).execute().asString()
+        }
+
+        /**
+         * 对线用语
+         */
+        fun getMa(): String {
+            val url = "https://nmsl.shadiao.app/api.php?level=min"
+            return HttpRequest.get(url).execute().asString()
+        }
+
+        /**
+         * 获取bilibili直播间信息
+         */
+        fun getBlibiliLiveStatus(bilibiliuid: String): JSONObject {
+            val url = "http://api.live.bilibili.com/room/v1/Room/getRoomInfoOld?mid=$bilibiliuid"
+            val body = HttpRequest.get(url).execute().asString()
+            return JSONObject.parseObject(body)
+        }
+
+        /**
+         * 获取setu
+         * @return
+         */
+        fun getSeTu(keyword: String): JSONObject? {
+            val url = "https://api.lolicon.app/setu/?apikey=165869815f1ecda7c45c44&keyword=$keyword"
+            val body = HttpRequest.get(url).execute().asString()
+            return JSON.parseObject(body)
+        }
+    }
+}
